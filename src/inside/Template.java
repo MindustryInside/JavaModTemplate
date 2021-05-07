b81dc299f47388c47bdc8d7748579c536b55e14f
summetdev <summet.dev@gmail.com>
2021-05-07 20:23:02 +0300

    template

---
+++ src/inside/Template.java
@@ -0,0 +1,14 @@
+package inside;
+
+import mindustry.mod.*;
+
+public class Template extends Mod {
+
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void loadContent() {
+    }
+}
